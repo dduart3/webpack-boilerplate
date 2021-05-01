7050bf8c3955c0a2f8b63a28d4acb3222b6a3c45
portareset1 <portareset179@gmail.com>
2021-05-01 14:04:38 -0400

    changes

--- src/script.js
+++
@@ -1,6 +0,0 @@
-import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import gsap from 'gsap';
-import iro from '@jaames/iro';
-
